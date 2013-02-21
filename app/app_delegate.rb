bd6687527fd871569eb66ac0205759220d6d0baa
Raymond T. Hightower <rth@wisdomgroup.com>
2013-02-21 17:31:52 -0600

    more cleanup for clarity

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,16 +1,9 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.makeKeyAndVisible
 
     intro_controller = IntroViewController.alloc.initWithNibName(nil, bundle: nil)
-    # nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
-
-    # tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle:nil)
-    # tab_controller.viewControllers = [nav_controller]
-
-    # @window.rootViewController = nav_controller
     @window.rootViewController = intro_controller
     
     true
--- app/controllers/intro_view_controller.rb
+++ app/controllers/intro_view_controller.rb
@@ -28,11 +28,9 @@ class IntroViewController < UIViewController
     @startbutton.setTitle("Start Game", forState:UIControlStateNormal)
     # @startbutton.setTitleColor(color, forState:UIControlStateNormal)
     @startbutton.sizeToFit
-    
     @startbutton.frame = [[self.view.frame.size.height / 2 - button_width / 2, self.view.frame.size.width / 2], [button_width, 30]]
     @startbutton.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin
     @startbutton.addTarget(self, action:"start_game", forControlEvents:UIControlEventTouchUpInside)
-
     self.view.addSubview(@startbutton)
   end
 

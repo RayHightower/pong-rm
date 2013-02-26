9f5b0b7e23e6b955a6806a052a4f2e40f0f92073
Raymond T. Hightower <rth@wisdomgroup.com>
2013-02-26 16:17:50 -0600

    control from the center, not from the edges

--- app/controllers/game_view_controller.rb
+++ app/controllers/game_view_controller.rb
@@ -3,10 +3,10 @@ class GameViewController < UIViewController
   def init
     super
     @ball_size = [20, 20]
-    @ball_location = [240, 160]
+    @ball_start = [240, 160]
     @paddle_size = [20, 100]
-    @left_paddle_position = [20, 50]
-    @right_paddle_position = [440, 150]
+    @left_paddle_start = [20, 50]
+    @right_paddle_start = [440, 150]
 
     self.view = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     self.view.backgroundColor = UIColor.blackColor
@@ -19,20 +19,23 @@ class GameViewController < UIViewController
   end
 
   def create_ball
-    @ball_view = UIView.alloc.initWithFrame [@ball_location, @ball_size]
+    @ball_view = UIView.alloc.initWithFrame [@ball_start, @ball_size]
     @ball_view.backgroundColor = UIColor.whiteColor
+    @ball_view.center = [240, 200]
     self.view.addSubview(@ball_view)
   end
 
   def create_left_paddle
-    @left_paddle_view = PaddleView.alloc.initWithFrame [@left_paddle_position, @paddle_size]
+    @left_paddle_view = PaddleView.alloc.initWithFrame [@left_paddle_start, @paddle_size]
     @left_paddle_view.backgroundColor = UIColor.whiteColor
+    @left_paddle_view.center = [20, 200]
     self.view.addSubview(@left_paddle_view)
   end
 
   def create_right_paddle
-    @right_paddle_view = PaddleView.alloc.initWithFrame [@right_paddle_position, @paddle_size]
+    @right_paddle_view = PaddleView.alloc.initWithFrame [@right_paddle_start, @paddle_size]
     @right_paddle_view.backgroundColor = UIColor.whiteColor
+    @right_paddle_view.center = [460, 160]
     self.view.addSubview(@right_paddle_view)
   end
 
@@ -42,16 +45,16 @@ class GameViewController < UIViewController
     @left_score.font = UIFont.boldSystemFontOfSize(35)
     @left_score.sizeToFit
     @left_score.textColor = UIColor.whiteColor
-    @left_score.backgroundColor = UIColor.blackColor
+    @left_score.backgroundColor = UIColor.clearColor
     @left_score.center = [120, 30]
     self.view.addSubview(@left_score)
-    
+
     @right_score = UILabel.alloc.initWithFrame(CGRectZero)
     @right_score.text = "0"
     @right_score.font = UIFont.boldSystemFontOfSize(35)
     @right_score.sizeToFit
     @right_score.textColor = UIColor.whiteColor
-    @right_score.backgroundColor = UIColor.blackColor
+    @right_score.backgroundColor = UIColor.clearColor
     @right_score.center = [360, 30]
     self.view.addSubview(@right_score)
   end

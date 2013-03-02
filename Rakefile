6448961e814db06b207fafe150d41cdbcb0e04d7
Raymond T. Hightower <rth@wisdomgroup.com>
2013-03-02 09:26:36 -0600

    wrestling with provisioning profiles

--- Rakefile
+++ Rakefile
@@ -6,8 +6,11 @@ Motion::Project::App.setup do |app|
   # Use `rake config' to see complete project settings.
   app.name = 'Pong-RM'
 
-  app.interface_orientations = [:landscape_left]
+  app.interface_orientations = [:landscape_left, :landscape_right, :portrait]
   app.files_dependencies 'app/controllers/game_view_controller.rb' => 'app/views/score_label.rb'
   app.icons = ["pong-rm.jpg"]
+
+  app.provisioning_profile = '/Users/rth/Library/MobileDevice/Provisioning Profiles/[string-of-numbers].mobileprovision'
+  app.codesign_certificate = 'iPhone Developer: Raymond T Hightower (Z9NP4QZ4AE)'
 
 end
--- app/controllers/game_view_controller.rb
+++ app/controllers/game_view_controller.rb
@@ -37,14 +37,14 @@ class GameViewController < UIViewController
 
   def create_right_paddle
     @right_paddle_view = PaddleView.alloc.initWithFrame [[420,50], @paddle_size]
-    @right_paddle_view.center = [460, 160] # if x>160+/-, the  right paddle  will not respond to touch events.
+    @right_paddle_view.center = [self.view.size.height-20, self.view.size.width/2] # if x>160+/-, the  right paddle  will not respond to touch events.
     @right_paddle_view.backgroundColor = UIColor.whiteColor
     self.view.addSubview(@right_paddle_view)
   end
 
   def create_left_paddle
     @left_paddle_view = PaddleView.alloc.initWithFrame [@object_start, @paddle_size]
-    @left_paddle_view.center = [20, 150]
+    @left_paddle_view.center = [20, self.view.size.width/2]
     @left_paddle_view.backgroundColor = UIColor.whiteColor
     self.view.addSubview(@left_paddle_view)
   end

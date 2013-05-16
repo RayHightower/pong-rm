10a990c16f8d235bdf6d3e3aa6123bc84e733717
Raymond T. Hightower <rth@wisdomgroup.com>
2013-05-15 23:49:33 -0500

    initial setup of motion-xray

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,6 +1,6 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window = Motion::Xray::XrayWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.makeKeyAndVisible
 
     intro_controller = IntroViewController.alloc.initWithNibName(nil, bundle: nil)

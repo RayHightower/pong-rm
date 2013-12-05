6809d5023c6f22a68c1d6f1d7f0edca77a0943fb
Raymond T. Hightower <rth@wisdomgroup.com>
2013-12-05 12:54:39 -0600

    better choice: do the experiments in a topic branch!

--- app/controllers/game_view_controller.rb
+++ app/controllers/game_view_controller.rb
@@ -6,8 +6,8 @@ class GameViewController < UIViewController
     @object_start = [20, 50]
     @paddle_size = [20, 100]
 
-    self.view = UIView.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    # self.view = UIView.alloc.initWithFrame([[0,0], [480,320]])
+    # self.view = UIView.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    self.view = UIView.alloc.initWithFrame([[0,0], [480,320]])
 
     # RGB 40, 99, 34 = UIColor.tennisCourtGreenColor :-)
     # RGB 0, 0, 205 = A shade of blue
